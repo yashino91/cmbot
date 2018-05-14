0f1e9ceee63749309b504d88dae652304d51f879
Stefan <stefanploner@gmx.at>
2018-05-14 11:43:21 +0200

    synchronize CoinMarketContainer

--- src/main/kotlin/at/chaoticbits/coinmarket/CoinMarketContainer.kt
+++ src/main/kotlin/at/chaoticbits/coinmarket/CoinMarketContainer.kt
@@ -1,18 +1,22 @@
 package at.chaoticbits.coinmarket
 
+import java.util.Collections.synchronizedList
+import java.util.Collections.synchronizedMap
+
 
 /**
  * Holding lists of symbols slugs and erc20 tokens
  */
 object CoinMarketContainer {
 
-    val coinListings: MutableList<Coin> = mutableListOf()
+    val coinListings: MutableList<Coin> = synchronizedList(mutableListOf())
 
-    val erc20Tokens: MutableMap<String, String> = mutableMapOf()
+    val erc20Tokens: MutableMap<String, String> = synchronizedMap(mutableMapOf())
 
 
     fun findCoins (query: String): List<Coin> =
-        CoinMarketContainer.coinListings
-                .filter { it -> it.symbol.startsWith(query, true) || it.name.startsWith(query, true) }
+        synchronized(CoinMarketContainer.coinListings) {
+            CoinMarketContainer.coinListings.filter { it -> it.symbol.startsWith(query, true) || it.name.startsWith(query, true) }
+        }
 
 }
