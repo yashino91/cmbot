e78bdde712eed4391dcdd008f65da3324ce0ff1e
Stefan Ploner <s.ploner@gmx.net>
2018-05-14 12:10:25 +0200

    remove testSuccessInitTelegramBot

--- src/test/kotlin/at/chaoticbits/MainTest.kt
+++ src/test/kotlin/at/chaoticbits/MainTest.kt
@@ -1,6 +1,5 @@
 package at.chaoticbits
 
-import at.chaoticbits.testdata.Config
 import org.junit.Assert
 import org.junit.ClassRule
 import org.junit.Test
@@ -14,16 +13,6 @@ class MainTest {
         @JvmField
         val environmentVariables: EnvironmentVariables = EnvironmentVariables()
 
-    }
-
-    @Test
-    fun testSuccessInitTelegramBot() {
-        environmentVariables.set("CMBOT_TELEGRAM_TOKEN", Config.testBotToken)
-        val botSession = initTelegramBot()
-
-        Assert.assertNotNull(botSession)
-
-        botSession?.stop()
     }
 
     @Test
